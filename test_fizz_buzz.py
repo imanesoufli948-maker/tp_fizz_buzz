96d7834625cdc4565459b07ae5fa82144279d4cd
imanesoufli948-maker <iimanesoufli@gmail.com>
2025-10-23 10:41:11 +0200

    edit founction test of add testing multiples number 3 for test passed 100%

--- test_fizz_buzz.py
+++ test_fizz_buzz.py
@@ -2,6 +2,10 @@ import pytest
 
 from fizz_buzz import fizz_buzz
 
-@pytest.mark.parametrize("n",[1,2,3])
-def test_fizz_buzz_1_return_fizz_buzz(n):
+@pytest.mark.parametrize("n",[1,2,4])
+def test_fizz_buzz_other_number_return_fizz_buzz(n):
     assert fizz_buzz(n) == "fizz buzz"
+
+@pytest.mark.parametrize("n",[3,6,9])
+def test_fizz_buzz_multiples_3_return_fizz(n):
+    assert fizz_buzz(n) == "fizz"
